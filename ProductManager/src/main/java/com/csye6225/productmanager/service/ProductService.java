f177d3f0df6c42d3d7d52a1b8eba39549390b81b
ZhengruiLu <lu.zhengr@northeastern.edu>
2023-03-10 19:49:27 -0800

    modified application

--- ProductManager/src/main/java/com/csye6225/productmanager/service/ProductService.java
+++ ProductManager/src/main/java/com/csye6225/productmanager/service/ProductService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class ProductService {
