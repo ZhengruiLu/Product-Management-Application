dd06317a89fe55ac13757bae362adc80e1f7a2a6
ZhengruiLu <lu.zhengr@northeastern.edu>
2023-03-28 17:19:29 -0700

    modify jar mvn package plugin output dir

--- src/main/java/com/csye6225/productmanager/ProductManagerApplication.java
+++ src/main/java/com/csye6225/productmanager/ProductManagerApplication.java
@@ -2,8 +2,6 @@ package com.csye6225.productmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class ProductManagerApplication {
