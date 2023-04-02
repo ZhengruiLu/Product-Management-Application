f1453dbb73b07e96a777054f5f78e74f15d1d243
ZhengruiLu <lu.zhengr@northeastern.edu>
2023-04-02 03:56:05 -0700

    add logger and metrics

--- ProductManager/src/main/java/com/csye6225/productmanager/controller/ProductController.java
+++ ProductManager/src/main/java/com/csye6225/productmanager/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.csye6225.productmanager.repository.ProductRepository;
 import com.csye6225.productmanager.service.CustomUserDetails;
 import com.csye6225.productmanager.service.ProductService;
 import com.csye6225.productmanager.service.UserService;
+import com.timgroup.statsd.StatsDClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class ProductController {
     @Autowired
@@ -31,32 +35,44 @@ public class ProductController {
     @Autowired
     private UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+    @Autowired
+    private StatsDClient statsDClient;
     @GetMapping(value = "/v1/product/{productId}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Product> getProductById(
             @PathVariable(value = "productId")Integer id
     ) {
-        Optional<Product> optionalProduct = repo.findById(id);
-        Product product;
+        statsDClient.incrementCounter("endpoint.homepage.http.get");
 
-        if (optionalProduct.isPresent()) {
-            product = optionalProduct.get();
-        } else {
-            return new ResponseEntity<Product>(HttpStatus.FORBIDDEN);
+        try {
+            Optional<Product> optionalProduct = repo.findById(id);
+            Product product;
+
+            if (optionalProduct.isPresent()) {
+                product = optionalProduct.get();
+            } else {
+                logger.warn("No product found with id: " + id);
+                return new ResponseEntity<Product>(HttpStatus.FORBIDDEN);
+            }
+
+            Product retProduct = new Product(
+                    product.getId(),
+                    product.getName(),
+                    product.getDescription(),
+                    product.getSku(),
+                    product.getManufacturer(),
+                    product.getQuantity(),
+                    product.getDate_added(),
+                    product.getDate_last_updated(),
+                    product.getOwnerUserId()
+            );
+
+            logger.info("Successfully retrieved product with id: " + id);
+            return new ResponseEntity<Product>(retProduct, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error retrieving product with id: " + id, e);
+            return new ResponseEntity<Product>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        Product retProduct = new Product(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getSku(),
-                product.getManufacturer(),
-                product.getQuantity(),
-                product.getDate_added(),
-                product.getDate_last_updated(),
-                product.getOwnerUserId()
-                );
-
-        return new ResponseEntity<Product>(retProduct, HttpStatus.OK);
     }
 
     @GetMapping(value = "/v1/product/{product_id}/image", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -64,12 +80,15 @@ public class ProductController {
             @PathVariable(value = "product_id")Integer product_id,
             Authentication authentication
     ) {
+        statsDClient.incrementCounter("endpoint.homepage.http.get");
+
         Optional<Product> optionalProduct = repo.findById(product_id);
         Product product;
 
         if (optionalProduct.isPresent()) {
             product = optionalProduct.get();
         } else {
+            logger.warn("Product with ID " + product_id + " not found");
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
@@ -78,10 +97,12 @@ public class ProductController {
         String currUserPassword = userDetails.getUser().getPassword();
 
         if (!currUserId.equals(product.getOwnerUserId()) || !currUserPassword.equals(product.getUser().getPassword())){
+            logger.warn("Unauthorized access to images of product with ID " + product_id);
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
         List<Image> images = service.getImagesById(product_id);
+        logger.info("Images of product with ID " + product_id + " retrieved successfully");
 
         return new ResponseEntity<>(images, HttpStatus.OK);
     }
@@ -91,11 +112,14 @@ public class ProductController {
             @PathVariable(value = "productId")Integer id,
             Authentication authentication
     ) {
+        statsDClient.incrementCounter("endpoint.homepage.http.delete");
+
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         Integer currUserId = userDetails.getUser().getId();
 
         //find user by id
         if (id == null || id < 0) {
+            logger.warn("Invalid ID " + id + " provided for product deletion");
             return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         }
 
@@ -105,13 +129,17 @@ public class ProductController {
         if (optionalProduct.isPresent()) {
             product = optionalProduct.get();
         } else {
+            logger.warn("Product with ID " + id + " not found");
             return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
         }
 
-        if (!currUserId.equals(product.getOwnerUserId()))
+        if (!currUserId.equals(product.getOwnerUserId())){
+            logger.warn("Unauthorized deletion of product with ID " + id);
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
 
         service.deleteById(id);
+        logger.info("Product with ID " + id + " deleted successfully");
 
         return new ResponseEntity<String>("Product delete successfully!", HttpStatus.NO_CONTENT);
     }
@@ -124,32 +152,40 @@ public class ProductController {
             @RequestParam(value ="sku", required = true)String sku,
             @RequestParam(value ="manufacturer", required = true)String manufacturer,
             @RequestParam(value ="quantity", required = true)Integer quantity
-            ) {
+    ) {
+        statsDClient.incrementCounter("endpoint.homepage.http.post");
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         User currUser = userDetails.getUser();
 
         Product product = new Product();
+        logger.info("Creating product with name: {}, description: {}, sku: {}, manufacturer: {}, and quantity: {}", name, description, sku, manufacturer, quantity);
 
         try {
             Integer currUserId = userDetails.getUser().getId();
             String currUserPassword = userDetails.getUser().getPassword();
 
             if (!currUserId.equals(product.getOwnerUserId()) || !currUserPassword.equals(product.getUser().getPassword())){
+                logger.warn("User with ID {} attempted to create a product for another user.", currUserId);
                 return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             if (quantity >= 0 && quantity <= 100)
                 product.setQuantity(quantity);
-            else
+            else{
+                logger.warn("Invalid quantity value {} provided while creating product.", quantity);
                 return new ResponseEntity<Product>(HttpStatus.BAD_REQUEST);
+            }
 
             if (name == null || name == ""
                     || description == null || description == ""
                     || sku == null || sku == ""
                     || manufacturer == null || manufacturer == ""
-            )
+            ) {
+                logger.warn("Invalid input provided while creating product.");
                 return new ResponseEntity<Product>(HttpStatus.BAD_REQUEST);
+            }
 
             product.setName(name);
             product.setDescription(description);
@@ -170,12 +206,12 @@ public class ProductController {
                     product.getDate_last_updated(),
                     product.getOwnerUserId()
             );
-
+            logger.info("Product created with SKU: {}", retProduct.getSku());
             return new ResponseEntity<Product>(retProduct, HttpStatus.CREATED);
         } catch (DataIntegrityViolationException ex) {
+            logger.error("Error creating product with SKU: {}, Error message: {}", product.getSku(), ex.getMessage());
             throw new DuplicateSkuException("Product with SKU " + product.getSku() + " already exists");
         }
-
     }
 
     @PutMapping(value = "/v1/product/{productId}", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -187,6 +223,12 @@ public class ProductController {
             @RequestParam(value = "manufacturer")String manufacturer,
             @RequestParam(value = "quantity")Integer quantity
     ) {
+
+        logger.info("Updating product with ID: {}", id);
+
+        statsDClient.incrementCounter("endpoint.homepage.http.put");
+
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         User currUser = userDetails.getUser();
@@ -199,36 +241,45 @@ public class ProductController {
         if (optionalProduct.isPresent()) {
             product = optionalProduct.get();
         } else {
+            logger.warn("Product with id {} not found.", id);
             return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
         }
 
-        if (!currUserId.equals(product.getOwnerUserId()))
+        if (!currUserId.equals(product.getOwnerUserId())) {
+            logger.warn("User with id {} is not authorized to update product with id {}.", currUserId, id);
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
 
         try {
             if (name == null
                     || description == null || sku == null || manufacturer == null || quantity == null
-            )
+            ) {
+                logger.warn("Product update failed due to invalid input. PUT requires all parameters");
                 return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            }
 
-                product.setName(name);
+            product.setName(name);
 
-                product.setDescription(description);
+            product.setDescription(description);
 
-                product.setSku(sku);
+            product.setSku(sku);
 
-                product.setManufacturer(manufacturer);
+            product.setManufacturer(manufacturer);
 
-                if (quantity >= 0 && quantity <= 100)
-                    product.setQuantity(quantity);
-                else
-                    return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            if (quantity >= 0 && quantity <= 100)
+                product.setQuantity(quantity);
+            else {
+                logger.warn("Product update failed due to invalid quantity input. Should be 0-100.");
+                return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            }
 
             repo.save(product);
 
+            logger.info("Product with id {} updated successfully.", id);
             return new ResponseEntity<String>("Product update successfully!", HttpStatus.NO_CONTENT);
         }
         catch (DataIntegrityViolationException ex) {
+            logger.error("Product update failed due to duplicate SKU.");
             throw new DuplicateSkuException("Product with SKU " + product.getSku() + " already exists");
         }
     }
@@ -243,6 +294,11 @@ public class ProductController {
             @RequestParam(value = "quantity", required = false)Integer quantity,
             Authentication authentication
     ) {
+        logger.info("Updating product with id {}", id);
+
+        statsDClient.incrementCounter("endpoint.homepage.http.patch");
+
+
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         User currUser = userDetails.getUser();
         Integer currUserId = currUser.getId();
@@ -254,17 +310,22 @@ public class ProductController {
         if (optionalProduct.isPresent()) {
             product = optionalProduct.get();
         } else {
+            logger.warn("Product with id {} not found", id);
             return new ResponseEntity<String>(HttpStatus.FORBIDDEN);
         }
 
-        if (!currUserId.equals(product.getOwnerUserId()))
+        if (!currUserId.equals(product.getOwnerUserId())) {
+            logger.warn("User with id {} is not the owner of product with id {}", currUserId, id);
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
 
         try {
             if (name == null
                     && description == null && sku == null && manufacturer == null && quantity == null
-            )
+            ){
+                logger.warn("No updates provided for product with id {}", id);
                 return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+            }
 
             //update user info
             if (name != null)
@@ -282,15 +343,19 @@ public class ProductController {
             if (quantity != null) {
                 if (quantity >= 0 && quantity <= 100)
                     product.setQuantity(quantity);
-                else
+                else {
+                    logger.warn("Product update failed due to invalid quantity input. Should be 0-100.");
                     return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+                }
             }
         } catch (DataIntegrityViolationException ex) {
+            logger.error("Product update failed due to duplicate SKU.");
             throw new DuplicateSkuException("Product with SKU " + product.getSku() + " already exists");
         }
 
         repo.save(product);
 
+        logger.info("Product with id {} updated successfully.", id);
         return new ResponseEntity<String>("Product updated successfully!", HttpStatus.NO_CONTENT);
     }
 }
